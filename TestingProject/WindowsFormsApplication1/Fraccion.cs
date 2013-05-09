2e6ed6f051e1359078a20496d06b883c0e95eaba
okapichevrolet <g.r.ramirezb@unitec.edu>
2013-05-09 11:17:47 -0600

    prototipos de nuevo

--- TestingProject/WindowsFormsApplication1/Fraccion.cs
+++ TestingProject/WindowsFormsApplication1/Fraccion.cs
@@ -8,6 +8,53 @@ namespace TestingProject
 {
     class Fraccion
     {
+        enum signo{
+            pos= 1,
+            neg= -1
+        } 
 
+        long num;
+        long den; 
+        signo sig;
+
+        static Fraccion simplificar(Fraccion f)
+        {
+            return f;
+        }
+
+        static bool isZero(Fraccion f)
+        {
+            return f.num == 0;
+        }
+
+        static bool sonIguales(Fraccion a, Fraccion b)
+        {
+            //por retornar un valor, luego lo cambio
+            return a.den == b.den && a.num == b.num &&a.sig==b.sig;
+        }
+
+        static Fraccion suma(Fraccion a, Fraccion b)
+        {
+            //Implementar
+            return null;
+        }
+
+        static Fraccion resta(Fraccion a, Fraccion b)
+        {
+            //Implementar
+            return null;
+        }
+
+        static Fraccion multi(Fraccion a, Fraccion b)
+        {
+            //Implementar
+            return null;
+        }
+
+        static Fraccion divi(Fraccion a, Fraccion b)
+        {
+            //Implementar
+            return null;
+        }
     }
 }
